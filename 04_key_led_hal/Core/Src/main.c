9b6e45229231720b637e5ebd7349e29edb686c47
WEI <wei030211@gmail>
2025-06-02 21:26:35 +0800

    04

--- 04_key_led_hal/Core/Src/main.c
+++ 04_key_led_hal/Core/Src/main.c
@@ -43,8 +43,7 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-uint8_t n = 3;
-uint16_t leds[3] = {LED1_Pin, LED2_Pin, LED3_Pin};
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -95,18 +94,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    // 循环3次，将leds数组中的前3个引脚置为低电平
-    for (uint8_t i = 0; i < 3; i++)
-    {
-      HAL_GPIO_WritePin(GPIOB, leds[i], GPIO_PIN_RESET);
-      HAL_Delay(500);
-    }
-    // 循环3次，将leds数组中的后3个引脚置为高电平
-    for (uint8_t i = 0; i < 3; i++)
-    {
-      HAL_GPIO_WritePin(GPIOB, leds[n - i - 1], GPIO_PIN_SET);
-      HAL_Delay(500);
-    }
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
